6d9024237ea548fed51ff73909d6d66b29234728
荆泽阳 <961940103qq@.com>
2017-03-14 09:25:00 +0800

    Initial commit

---
+++ app/src/main/java/com/baway/jingzeyang/Mygetitem.java
@@ -0,0 +1,12 @@
+package com.baway.jingzeyang;
+
+/**
+ * Created by llw on 2016/4/8.
+ */
+public class Mygetitem {
+    String srt = null;
+
+    public Mygetitem(String srt) {
+        this.srt = srt;
+    }
+}
